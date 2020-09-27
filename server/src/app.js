8569ecbbe8d1f6855335ef2c56f4b1d649ad2565
Max Oosterveen <maxoosterveen@gmail.com>
2020-09-27 10:03:57 +0200

    added static files for when the server is running in prod

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const middlewares = require('./middlewares');
 const cron = require('node-cron');
+const path = require('path');
+const middlewares = require('./middlewares');
 
 const apiRouter = require('./api/Api.router');
 
@@ -11,33 +12,37 @@ const app = express();
 
 require('dotenv').config();
 
-const initalizeMongoDB = async () => {
+// Connect to MongoDB database
+(async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
+    console.log('MongoDB is running');
   } catch (error) {
     console.log(error);
   }
+})();
+
+// Serve static react files if env is production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
 }
 
-initalizeMongoDB();
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World!',
-  });
-});
-
+// append API router to default API url
 app.use('/api/v1', apiRouter);
 
+// Run the function when the server starts
 handleFetchData();
+
+// Fetch the data every 5 minutes
 cron.schedule('*/5 * * * *', () => {
   handleFetchData();
 });
 
+// Middlewares
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
